166c00abf6f18f2b63a5cdeb952051e715f9739e
Thomas Neder <thomas.neder@freenet.de>
2023-10-31 16:15:35 +0100

    Add HalfBandFilter class

---
+++ src/support/halfbandfilter.cpp
@@ -0,0 +1,5 @@
+//
+// Created by work on 31/10/23.
+//
+
+#include "halfbandfilter.h"
---
+++ src/support/halfbandfilter.h
@@ -0,0 +1,15 @@
+//
+// Created by work on 31/10/23.
+//
+
+#ifndef DABSTAR_HALFBANDFILTER_H
+#define DABSTAR_HALFBANDFILTER_H
+
+
+class HalfBandFilter
+{
+
+};
+
+
+#endif //DABSTAR_HALFBANDFILTER_H
