001a81fbb13f8310603f8da3624f6be8ad7fff5e
Thomas Neder <thomas.neder@freenet.de>
2023-11-19 20:47:54 +0100

    Simplify device exception handling 2

--- src/devices/device-exceptions.h
+++ src/devices/device-exceptions.h
@@ -1,22 +1,21 @@
 
-#include	<cstdio>
+#include <string>
 #include <stdexcept>
-
-#define MAX_MESSAGE_SIZE 256
 
 class std_exception_string : public std::exception
 {
-private:
-  char text[MAX_MESSAGE_SIZE];
 public:
-  std_exception_string(const std::string & s)
+  explicit std_exception_string(const std::string & s)
   {
-    snprintf(text, MAX_MESSAGE_SIZE, "%s ", s.c_str());
+    mText = s;
   }
 
   [[nodiscard]] const char * what() const noexcept override
   {
-    return text;
+    return mText.c_str();
   }
+
+private:
+  std::string mText;
 };
 
