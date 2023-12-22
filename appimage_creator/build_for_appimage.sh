a5fed97a9bb1314db3764926604fb15eb4276444
Thomas Neder <thomas.neder@freenet.de>
2023-12-22 04:20:34 +0100

    Set version to 2.0.0

--- appimage_creator/build_for_appimage.sh
+++ appimage_creator/build_for_appimage.sh
@@ -2,13 +2,14 @@
 
 SRC=$PWD/..
 DST=~/DABstarAppImage/build
+CMFLAG=-DCONV_IN_FILES=OFF #switch off .in-files conversion as repo access could only be read-only
 echo "Source folder is $SRC"
 echo "Destination folder is $DST"
 rm -rf $DST
 mkdir -p $DST
 cd $DST || exit
 
-cmake $SRC -DAIRSPY=ON -DSDRPLAY_V2=ON -DSDRPLAY_V3=ON -DHACKRF=ON -DLIMESDR=ON -DRTL_TCP=ON -DPLUTO=ON -DUHD=ON -DRTLSDR_LINUX=ON -DUSE_HBF=OFF -DDATA_STREAMER=OFF -DVITERBI_SSE=ON -DVITERBI_NEON=OFF -DFDK_AAC=OFF
+cmake $SRC $CMFLAG -DAIRSPY=ON -DSDRPLAY_V2=ON -DSDRPLAY_V3=ON -DHACKRF=ON -DLIMESDR=ON -DRTL_TCP=ON -DPLUTO=ON -DUHD=ON -DRTLSDR_LINUX=ON -DUSE_HBF=OFF -DDATA_STREAMER=OFF -DVITERBI_SSE=ON -DVITERBI_NEON=OFF -DFDK_AAC=OFF
 
 if [ $? -eq 0 ]; then
     echo "cmake runs successfully, now we begin to build the application"
