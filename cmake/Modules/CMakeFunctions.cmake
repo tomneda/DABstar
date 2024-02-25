de8dc200c02616b1934e7d7e7192c32afa3d0947
tomneda <103888527+tomneda@users.noreply.github.com>
2024-02-25 01:32:51 +0100

    Add function to get git commit hash in CMakeFunctions.cmake

--- cmake/Modules/CMakeFunctions.cmake
+++ cmake/Modules/CMakeFunctions.cmake
@@ -10,3 +10,15 @@ function(print_env)
     message("${var} = $ENV{${var}}")
   endforeach ()
 endfunction()
+
+function(get_git_commit_hash)
+  if (EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/.git" AND IS_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}/.git")
+    execute_process(
+            COMMAND git rev-parse --short HEAD
+            WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+            OUTPUT_VARIABLE ARG_GIT_COMMIT_HASH
+            OUTPUT_STRIP_TRAILING_WHITESPACE
+    )
+    set(GIT_COMMIT_HASH ${ARG_GIT_COMMIT_HASH} PARENT_SCOPE)
+  endif ()
+endfunction()
