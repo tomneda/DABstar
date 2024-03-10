ddf4830ee4b12605d47b0feac722072867a4bc79
tomneda <103888527+tomneda@users.noreply.github.com>
2024-03-10 19:22:26 +0100

    Adaption for VS build 2. Compiles and links but is not working

--- cmake/Modules/CMakeFunctions.cmake
+++ cmake/Modules/CMakeFunctions.cmake
@@ -5,11 +5,24 @@ function(print)
   endforeach ()
 endfunction()
 
+
 function(print_env)
   foreach (var ${ARGN})
     message("${var} = $ENV{${var}}")
   endforeach ()
 endfunction()
+
+
+function(print_all_vars argVarStart)
+  get_cmake_property(_variableNames VARIABLES)
+  foreach (_variableName ${_variableNames})
+    string(REGEX MATCH "^${argVarStart}" _isMatched ${_variableName})
+    if (_isMatched)
+      message("*** ${_variableName}=${${_variableName}}")
+    endif ()
+  endforeach ()
+endfunction()
+
 
 function(get_git_commit_hash)
   if (EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/.git" AND IS_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}/.git")
@@ -22,3 +35,29 @@ function(get_git_commit_hash)
     set(GIT_COMMIT_HASH ${ARG_GIT_COMMIT_HASH} PARENT_SCOPE)
   endif ()
 endfunction()
+
+
+macro(search_for_library argLibName)
+  pkg_check_modules(${argLibName} ${argLibName})
+  if (${argLibName}_FOUND)
+    link_directories(${${argLibName}_LIBRARY_DIRS})
+  else ()
+    find_package(${argLibName})
+    if (NOT ${argLibName}_FOUND)
+      message(FATAL_ERROR "Please install ${argLibName}")
+    endif ()
+  endif ()
+  list(APPEND extraLibs ${${argLibName}_LIBRARIES})
+  #list(APPEND extraLibs ${${argLibName}_LIBRARY})
+  include_directories(${${argLibName}_INCLUDE_DIRS})
+  print_all_vars(${argLibName})
+endmacro()
+
+
+macro(try_find_vcpkg_toolset_path)
+  if (DEFINED ENV{PKG_ROOT} AND NOT DEFINED CMAKE_TOOLCHAIN_FILE)
+    # or at cmake prompt: -DCMAKE_TOOLCHAIN_FILE=c:\vcpkg\scripts\buildsystems\vcpkg.cmake
+    set(CMAKE_TOOLCHAIN_FILE "$ENV{PKG_ROOT}/scripts/buildsystems/vcpkg.cmake" CACHE STRING "")
+    message("Automatically set toolchain file to ${CMAKE_TOOLCHAIN_FILE}")
+  endif ()
+endmacro()
