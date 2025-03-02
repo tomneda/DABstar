b52143baf81470b62b6a8e717bc7bfffd0778159
Thomas Neder <103888527+tomneda@users.noreply.github.com>
2025-03-02 22:41:11 +0100

    Adapt AppImage make scripts

--- appimage_creator/cmake_and_build_for_appimage.sh
+++ appimage_creator/cmake_and_build_for_appimage.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
+QT_VERSION=6.8.2
 CUD=$PWD
 SRC=$CUD/..
 DST=~/DABstarAppImage/build
 CMFLAG=-DCONV_IN_FILES=OFF #switch off .in-files conversion as repo access could only be read-only
-QtPath=-DCMAKE_PREFIX_PATH=/home/workvm/fs_Qt/6.8.2/gcc_64/
+QtPath=-DCMAKE_PREFIX_PATH=/home/workvm/fs_Qt/$QT_VERSION/gcc_64/
 echo "Source folder is $SRC"
 echo "Destination folder is $DST"
 rm -rf $DST
 mkdir -p $DST
 cd $DST || exit
 
-cmake $SRC $CMFLAG $QtPath -DUSE_SIMD=ON -DUSE_LTO=ON -DAIRSPY=ON -DSDRPLAY_V2=ON -DSDRPLAY_V3=ON -DHACKRF=ON -DLIMESDR=ON -DRTL_TCP=ON -DPLUTO=ON -DUHD=ON -DRTLSDR=ON -DUSE_HBF=OFF -DDATA_STREAMER=OFF -DVITERBI_SSE=ON -DVITERBI_NEON=OFF -DFDK_AAC=ON
+cmake $SRC $CMFLAG $QtPath -DSSE_OR_AVX=ON -DUSE_LTO=ON -DAIRSPY=ON -DSDRPLAY_V2=ON -DSDRPLAY_V3=ON -DHACKRF=ON -DLIMESDR=ON -DRTL_TCP=ON -DPLUTO=ON -DUHD=ON -DRTLSDR=ON -DUSE_HBF=OFF -DDATA_STREAMER=OFF -DVITERBI_SSE2=ON -DVITERBI_NEON=OFF -DFDK_AAC=ON
 
 if [ $? -eq 0 ]; then
     echo "cmake runs successfully, now we begin to build the application"
