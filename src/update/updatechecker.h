cbce8b9c5ab3cf68909812b02f29ea9a8043ea30
Thomas Neder <103888527+tomneda@users.noreply.github.com>
2025-12-19 00:38:02 +0100

    Add update checker and update dialog, bump version to 4.5.1

---
+++ src/update/updatechecker.h
@@ -0,0 +1,58 @@
+/*
+ * This file is part of the AbracaDABra project
+ *
+ * MIT License
+ *
+ * Copyright (c) 2019-2025 Petr Kopecký <xkejpi (at) gmail (dot) com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#ifndef UPDATECHECKER_H
+#define UPDATECHECKER_H
+
+#include <QNetworkAccessManager>
+#include <QNetworkReply>
+#include <QObject>
+
+class UpdateChecker : public QObject
+{
+    Q_OBJECT
+public:
+    explicit UpdateChecker(QObject *parent = nullptr);
+    ~UpdateChecker();
+    void check();
+    QString version() const;
+    bool isPreRelease() const;
+    QString releaseNotes() const;
+
+signals:
+    void finished(bool result);
+
+private:
+    QNetworkAccessManager *m_netAccessManager = nullptr;
+    QString m_version;
+    bool m_isPreRelease;
+    QString m_releaseNotes;
+
+    void onFileDownloaded(QNetworkReply *reply);
+    bool parseResponse(const QByteArray &data);
+};
+
+#endif  // UPDATECHECKER_H
